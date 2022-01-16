0ceaed1e99feb506396f8e732a637d45d683e31c
Igor <i_swat@mail.ru>
2022-01-17 00:08:50 +0300

    Скорректирован код с датой! И он все равно не работает!

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,6 +1,20 @@
 import {renderBlock} from './lib.js'
 
 export function renderSearchFormBlock(dateCheckIn: string, dateExit: string) {
+  const dateOne = new Date(dateCheckIn);
+  dateOne.setDate(dateOne.getDate() + 1);
+  const dateTwo = new Date(dateExit);
+  const maxDate = new Date(dateOne.getFullYear(), dateOne.getMonth() + 2, 0);
+
+  const textDateOne = `0${dateOne.getDay()}-0${dateOne.getMonth()}-${dateOne.getFullYear()}`;
+  const textDateTwo = `0${dateTwo.getDay()}-0${dateTwo.getMonth()}-${dateTwo.getFullYear()}`;
+  const textMaxDate = `0${maxDate.getDay()}-0${maxDate.getMonth()}-${maxDate.getFullYear()}`;
+  // const numPrice = new Date(document.getElementById('check-out-date').value - document.getElementById('check-in-date').value)
+  //
+  // console.log(document.getElementById('check-out-date').value)
+  // console.log(document.getElementById('check-in-date').value)
+  console.log('dateOne.toLocaleDateString() => ' + dateOne.toLocaleDateString());
+
   renderBlock(
     'search-form-block',
     `
@@ -22,14 +36,21 @@ export function renderSearchFormBlock(dateCheckIn: string, dateExit: string) {
           <div class="row">
             <div>
                 <label for="check-in-date">Дата заезда</label>
-                <input id="check-in-date" type="date" value="2021-05-11" min="" max="" name="checkin" />
+                <input id="check-in-date" type="date" value="${dateOne}" min="${dateOne.toLocaleDateString()}" max="${maxDate.toLocaleDateString()}" name="checkin" />
+<!--                <input id="check-in-date" type="date" value="${dateOne.toLocaleDateString()}" min="${dateOne.toLocaleDateString()}" max="${maxDate.toLocaleDateString()}" name="checkin" />-->
+<!--                <input id="check-in-date" type="date" value="${dateOne}" min="${dateOne}" max="${maxDate}" name="checkin" />-->
+<!--                <input id="check-in-date" type="date" value="${textDateOne}" min="${textDateOne}" max="${textMaxDate}" name="checkin" />-->
               </div>
               <div>
                 <label for="check-out-date">Дата выезда</label>
-                <input id="check-out-date" type="date" value="2021-05-13" min="" max="" name="checkout" />
+                <input id="check-out-date" type="date" value="${dateTwo}" min="${dateOne.toLocaleDateString()}" max="${maxDate.toLocaleDateString()}"" name="checkout" />
+<!--                <input id="check-out-date" type="date" value="${dateTwo.toLocaleDateString()}" min="${dateOne.toLocaleDateString()}" max="${maxDate.toLocaleDateString()}"" name="checkout" />-->
+<!--                <input id="check-out-date" type="date" value="${dateTwo}" min="${dateOne}" max="${maxDate}"" name="checkout" />-->
+<!--                <input id="check-out-date" type="date" value="${textDateTwo}" min="${textDateOne}" max="${textMaxDate}"" name="checkout" />-->
               </div>
               <div>
                 <label for="max-price">Макс. цена суток</label>
+<!--                <input id="max-price" type="text" value="$...{numPrice}" name="price" class="max-price" />-->
                 <input id="max-price" type="text" value="" name="price" class="max-price" />
               </div>
               <div>
@@ -40,16 +61,4 @@ export function renderSearchFormBlock(dateCheckIn: string, dateExit: string) {
     </form>
     `
   )
-
-  const currentDate = new Date();
-  const dateOne = new Date(dateCheckIn);
-  dateOne.setDate(dateOne.getDate() + 1);
-  const dateTwo = new Date(dateExit);
-  const maxDate = new Date(dateOne.getFullYear(), dateOne.getMonth() + 2, 0);
-
-  let dateControl = document.getElementById('check-in-date');
-
-  console.log(`dateOne => ${dateOne}`)
-  console.log('dateControl')
-  console.log(dateControl)
 }
